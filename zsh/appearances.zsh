afa747954b8841c85bc97620b8fe0090d40cf465
Jia Ching Jian <sagiters1128@gmail.com>
2015-09-26 14:33:56 +0800

    modify ls background color

--- zsh/appearances.zsh
+++ zsh/appearances.zsh
@@ -49,7 +49,7 @@ function terminal_title_cwd () {
 # =========
 autoload -U colors && colors
 export LSCOLORS="exfxcxdxbxegedabagacad"
-export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+export LS_COLORS="di=34;49:ln=35;49:so=32;49:pi=33;49:ex=31;49:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
 export GREP_COLOR="1;33"
 
 # Find the option for using colors in ls, depending on the version: Linux or BSD
